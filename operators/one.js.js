c6e26d60bedc96e0784135b0ea77c796c83e921e
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-04 23:23:39 +0530

    Operator

---
+++ operators/one.js.js
@@ -0,0 +1,16 @@
+//Arthamatic Operator
+
+var a = 1 ;
+var b = 2 ;
+var c = '3';
+var d ="Hello"
+
+console.log(a + b);
+console.log(a * b);
+console.log(a - b);
+console.log(a % b);
+console.log(a + c);
+console.log(a * c);
+console.log(a * d);
+
+
---
+++ operators/three.js
@@ -0,0 +1,14 @@
+// relational Operator
+
+
+var sal = 45000;
+var result = sal > 40000
+
+console.log(result);
+
+if (sal > 45000){
+    console.log("Eligible for Marriage");
+}
+else {
+    console.log("Not eligible")
+}
