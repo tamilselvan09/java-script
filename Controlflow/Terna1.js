98e30031489da2bcdb500f1622d6b127445a9e51
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-09 22:36:43 +0530

    con

---
+++ Controlflow/Terna1.js
@@ -0,0 +1,11 @@
+var sal =45000;
+
+sal >40000 ? console.log("Marriage")  : console.log("Not, Marriage")
+
+
+if (sal >50000) {
+    console.log("Marriage")
+}
+else{
+    console.log("Not,Marriage")
+}
---
+++ Controlflow/Terna2.js
@@ -0,0 +1,7 @@
+"RajiniKanth" ? console.log("100 Times")  : console.log("0 Times")
+    //True  because string also have some value
+
+
+0 ? console.log("100 Times")  : console.log("0 Times")
+
+   //false because 0 have no value
