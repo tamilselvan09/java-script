faa1ce2094c7711b0e25b330ccc92cd2d47d3e63
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-10 10:50:53 +0530

    for

---
+++ Iterative flow/for.js
@@ -0,0 +1,6 @@
+// wap to print 1 to 5
+
+
+for (var i = 1; i<=5; i = i+1) {
+    console.log(i)
+}
---
+++ Iterative flow/for1.js
@@ -0,0 +1,8 @@
+// first 10 even number
+
+
+for (var i = 0; i<=20; i= i+2){
+    console.log(i)
+}
+
+
