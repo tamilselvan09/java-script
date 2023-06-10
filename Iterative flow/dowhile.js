fbcdfd6ba1d2080e8af83cadd6609f1cb6235111
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-10 11:10:46 +0530

    do

---
+++ Iterative flow/dowhile.js
@@ -0,0 +1,16 @@
+let i = 1;
+do{
+    console.log(i);
+    i = i+1
+}
+while(i<=10)
+
+
+
+
+
+ for(let i = 1; i <=10; i = i+ 1){
+    console.log(i)
+
+ }
+
---
+++ Iterative flow/while.js
@@ -0,0 +1,13 @@
+//10,20,30,40,50,60,70,80,90,
+
+/*
+for (var i =10; i<100; i=i+10){
+    console.log(i)
+}
+*/
+
+var i = 10;
+while (i<=100) {
+    console.log(i)
+    i =i +10
+}
