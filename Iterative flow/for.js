c3dad2346158f3273ac2e761dcbdf58fb49659a8
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-10 10:56:38 +0530

    for

--- Iterative flow/for.js
+++ Iterative flow/for.js
@@ -1,5 +1,9 @@
 // wap to print 1 to 5
 
+i= 1;     //instilasion
+i <= 5 ;  // contision
+i++  ;    // incre
+
 
 for (var i = 1; i<=5; i = i+1) {
     console.log(i)
---
+++ Iterative flow/for2.js
@@ -0,0 +1,6 @@
+// 10 odd number
+
+
+for (var i=1; i<=19; i=i+2){
+  console.log(i)
+}
