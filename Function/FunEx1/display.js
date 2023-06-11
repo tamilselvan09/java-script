faa0e402852233a736b92307dda0e943108c056c
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-11 16:00:23 +0530

    func

---
+++ Function/FunEx1/display.js
@@ -0,0 +1,4 @@
+function display(){
+    console.log("Hello, GM")
+    alert("Go and Study")
+}
--- Function/nine.js
+++ Function/nine.js
@@ -1,6 +1,14 @@
+//create once use ennumber of time
+
 function add (a, b) {
     return a +b
 }
 
 let result = add (100, 200);
  console.log(result);          //300
+
+
+
+
+ let result1 = add (1, 2);
+ console.log( result1);          //3
