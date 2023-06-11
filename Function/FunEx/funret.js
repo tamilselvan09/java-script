d7053d66b69be63dfb16e83c6c08d90de2555d4c
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-11 15:56:32 +0530

    fun

---
+++ Function/FunEx/funret.js
@@ -0,0 +1,17 @@
+
+function wish () {
+    return "Hello"
+}
+
+let message = wish ();
+console.log(message)     //Hello
+
+
+
+
+function wish1 (msg) {
+    return "Hello" + msg
+}
+
+let message1 = wish1 ("Good Morning");
+console.log(message1)  
---
+++ Function/FunEx/main.js
@@ -0,0 +1,18 @@
+function wish () {
+   return "hello";
+    console.log("hello,gm")
+}  // after writting return statement it's called unreached statement so line no 3 will not excicute
+let message  = wish ();
+console.log(message)
+
+
+
+
+
+function wish1 () {
+    
+     console.log("hello,gm")
+     return "hello";
+ }  
+ let message1  = wish1 ();
+ console.log(message1)
