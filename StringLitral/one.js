09ceeee453c315e58eefcc5bb6d68ce5f2abab53
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-19 23:44:01 +0530

    litral

---
+++ StringLitral/one.js
@@ -0,0 +1,7 @@
+var name  = "Rahul Gandhi"
+var sal = 45000
+console.log(`${name}`)  //RAhul Gandhi
+
+console.log("Employee Name:" + name + " and Employee Salary:" + sal)  //Employee Name:Rahul Gandhi and Employee Salary:45000
+
+console.log(`Employee Name:${name} and Employee Salary:${sal}`); //Employee Name:Rahul Gandhi and Employee Salary:45000
